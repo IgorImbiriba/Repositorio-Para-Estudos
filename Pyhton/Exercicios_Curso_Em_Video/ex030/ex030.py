1e680222f682b6cd43f2da0f3dfee2619f902e14
Igor C. Imbiriba <igorcasemiro@hotmail.com>
2022-09-23 19:41:04 -0300

    EXERCICIO 30 E 31 CURSO EM VIDEO

---
+++ Pyhton/Exercicios_Curso_Em_Video/ex030/ex030.py
@@ -0,0 +1,7 @@
+print('Este programa verifica se um numero é \'par\' ou \'impar\' ')
+num = int(input('digite um numero: '))
+
+if num % 2 == 0:  # verificando se é par
+    print('O numero que você digitou é PAR')
+else:
+    print('O numero que voce digitou é IMPAR')
---
+++ Pyhton/Exercicios_Curso_Em_Video/ex031/ex031.py
@@ -0,0 +1,31 @@
+###################################################
+#             Metodo if tradicional
+"""
+distancia = float(input('Qual é a distancia da sua viagem? '))
+print('Você está prestes a começar uma viajem de {} km'.format(distancia))
+if distancia <= 200:
+    preco = distancia * 0.50
+else:
+    preco = distancia * 0.45
+print('E o preço da sua viajem será de R${:.2f}'.format(preco))
+"""
+###################################################
+#               Metodo if simplificado
+"""
+distancia = float(input('Qual é a distancia da sua viagem? '))
+print('Você está prestes a começar uma viajem de {} km'.format(distancia))
+preco = distancia * 0.50 if distancia <=200 else distancia * 0.45
+print('E o preço da sua viajem será de R${:.2f}'.format(preco))
+"""
+####################################################
+print('Vamos calcular o preço da sua passagem')
+dist = float(input('Qual a distancia, em km, da sua viagem? '))
+
+if dist <= 200:
+    val = 0.50 * dist
+    print('Voce vai viajar por {:.2f} KM, a passagem vai custar R${:.2f}'
+          .format(dist, val))
+else:
+    val = 0.45 * dist
+    print('Voce vai viajar por {:.2f} km, a passagem vai custar R${:.2f}'
+          .format(dist, val))  
