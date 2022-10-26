15f9232d28f21bc6b5f0b51dc6c42f0eaadcbe0c
Igor C. Imbiriba <igorcasemiro@hotmail.com>
2022-10-25 22:39:07 -0300

    Aula TestaValores

---
+++ Java/eclipse-workspace/sintaxe_variaveis_e_fluxo/src/TestaValores.java
@@ -0,0 +1,14 @@
+
+public class TestaValores {
+		public static void main(String[] args) { //main -> ctrl + espaço
+			int primeiro = 5;
+			int segundo = 7;
+			segundo = primeiro;
+			primeiro = 10;
+			
+			// valor de segundo?
+			
+			System.out.println(segundo);
+			
+		}
+}
